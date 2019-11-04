6ac158d3096f6539b274015df5fe8485656c7130
xiaotao.li 恒拓开源_李小涛 <lixiaotao@foreveross.com>
2019-11-04 20:54:48 +0800

    order模块

---
+++ order/src/main/java/com/lxt/controller/OrderController.java
@@ -0,0 +1,18 @@
+package com.lxt.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/index")
+    public String index() {
+        return this.port;
+    }
+}
